03e07bf35eeaab85804259f1b482907ceca9fc29
Fabiano Gonzalez <fabianolgonzales@gmail.com>
2016-06-16 12:20:30 +0200

    Show post user

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :posts
-  
+
   get 'posts/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
